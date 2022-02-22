fe86d17667e67818a6591255422c515faa57fed1
andrewcantrell <andyroocantrell@gmail.com>
2022-02-22 14:22:47 -0800

    commit

---
+++ src/EGR425_Phase2_main_i2c_scanner.cpp
@@ -0,0 +1,18 @@
+#include <M5Core2.h>
+
+// Variables
+
+
+
+// Setup code here
+void setup(){
+    M5.begin();
+
+    Serial.println("Status: scanning for I2c devices...");
+}
+
+
+// Looping code
+void loop(){
+
+}
